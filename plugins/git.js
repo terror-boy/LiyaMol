992441172923f3b189deb882670c6e739061a99d
sachusamu-c <87406580+sachusamu-c@users.noreply.github.com>
2021-09-05 22:11:06 +0530

    𝘴𝘴 ᴄͥʀⷮᴇᷤᴀⷨᴛⷷɪᴏɴ

--- plugins/git.js
+++ plugins/git.js
@@ -1,45 +1,34 @@
- 
-
 const Asena = require('../events');
-
 const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
-
 const axios = require('axios');
 
 const Language = require('../language');
+const Lang = Language.getString('gitlink');
 
-const Lang = Language.getString('wallpaper');
-
-Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message, match) => {
+Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.GL}, (async (message, match) => {
 
     var r_text = new Array ();
-
     
-
     
-
    
-
-  r_text[0] = "https://ibb.co/5n9gdxj";
-
+  r_text[0] = "https://i.ibb.co/9rrnJ7g/20210815-080839.jpg";
     
-
     
-
     var i = Math.floor(1*Math.random())
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*creater SACHUSAMU*
-
-*owner number wa.me/919744474996*
-
-*bot setting video : https://youtu.be/RlSG4ZgPls0*
-
-*githublink : https://github.com/sachusamu-c/JINN*
-
-*audio commads : https://github.com/sachusamu-c/JINN/tree/master/uploads*
-
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {quoted: message.data , mimetype: Mimetype.png, caption: `*≈≈≈≈≈≈≈Links ☟︎︎︎≈≈≈≈≈≈≈≈*
+ 
+        *Github link        _https://tinyurl.com/yggyjfgf_*
+ 
+       *Audio commads-1     _https://tinyurl.com/yfdkmuhd_*
+ 
+ 
+       *Audio commads-2     _https://tinyurl.com/yejp9qkh_*
+  
+  
+      *Sticker commads      _https://tinyurl.com/yejlp33d_*
 `}) 
 
-}));
+})); 
