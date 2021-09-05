a02b3b7692deb027b3cde049ff92c0731b7dcc38
sachusamu-c <87406580+sachusamu-c@users.noreply.github.com>
2021-09-05 22:40:33 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -1,34 +1,15 @@
-const Asena = require('../events');
-const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
-const axios = require('axios');
-
-const Language = require('../language');
-const Lang = Language.getString('gitlink');
-
-Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.GL}, (async (message, match) => {
-
-    var r_text = new Array ();
-    
-    
-   
-  r_text[0] = "https://i.ibb.co/9rrnJ7g/20210815-080839.jpg";
-    
-    
-    var i = Math.floor(1*Math.random())
-
-    var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
-
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {quoted: message.data , mimetype: Mimetype.png, caption: `*≈≈≈≈≈≈≈Links ☟︎︎︎≈≈≈≈≈≈≈≈*
- 
-        *Github link        _https://tinyurl.com/yggyjfgf_*
- 
-       *Audio commads-1     _https://tinyurl.com/yfdkmuhd_*
- 
- 
-       *Audio commads-2     _https://tinyurl.com/yejp9qkh_*
-  
-  
-      *Sticker commads      _https://tinyurl.com/yejlp33d_*
+const Asena=require('\x2E\x2E\x2F\x65\x76\x65\x6E\x74\x73');//0
+const {MessageType,MessageOptions,Mimetype}=require('\x40\x61\x64\x69\x77\x61\x6A\x73\x68\x69\x6E\x67\x2F\x62\x61\x69\x6C\x65\x79\x73');//1
+const axios=require('\x61\x78\x69\x6F\x73');//2
+const Language=require('\x2E\x2E\x2F\x6C\x61\x6E\x67\x75\x61\x67\x65');//4
+const Lang=Language.getString('\x67\x69\x74\x6C\x69\x6E\x6B');//5
+Asena.addCommand({pattern:'\x67\x69\x74',fromMe:false,desc:Lang.GL},(async (_0x171D5,_0x17177)=>
+{
+	var _0x17233= new Array();//9
+	_0x17233[0]= "\x68\x74\x74\x70\x73\x3A\x2F\x2F\x61\x76\x61\x74\x61\x72\x73\x2E\x67\x69\x74\x68\x75\x62\x75\x73\x65\x72\x63\x6F\x6E\x74\x65\x6E\x74\x2E\x63\x6F\x6D\x2F\x75\x2F\x38\x35\x36\x36\x34\x39\x33\x36\x3F\x76\x3D\x34";var _0x17119=Math.floor(1* Math.random());//16
+	var _0x17291= await axios["\x67\x65\x74"](`${_0x17233[_0x17119]}`,{responseType:'\x61\x72\x72\x61\x79\x62\x75\x66\x66\x65\x72'});//18
+	 await _0x171D5.sendMessage(Buffer(_0x17291.data),MessageType.image,{mimetype:Mimetype.png,caption:`*𝘴𝘴 ᴄͥʀⷮᴇᷤᴀⷨᴛⷷɪᴏɴ ᴄʀᴇᴀᴛᴇᴅ ʙʏ SACHUSAMU-ᴛᴇʀʀᴏʀ-ʙᴏʏ*
+*owner number wa.me/919778042644* \n\n *ABOUT ME          _https://github.com/terror-boy_* \n\n *Github link       _https://github.com/terror-boy/WhiteDevil_* \n\n *Audio commads-1    _https://github.com/terror-boy/WhiteDevil/tree/master/uploads_* \n\n *Audio commads-2    _https://github.com/terror-boy/WhiteDevil/tree/master/upload_* \n\n *Sticker commads  _https://github.com/terror-boy/WhiteDevil/tree/master/stickers_* \n\n *Video Commands   _https://github.com/terror-boy/WhiteDevil/tree/master/video_* \n\n *Bot Setting malayalam* _:_ *http://imgur.com/a/alPJ0fY.mp4*
 `}) 
 
-})); 
+}));
