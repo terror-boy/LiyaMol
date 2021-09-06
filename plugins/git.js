42f35ced69ff7357030947db62a2eb8f3ec19d03
sachusamu-c <87406580+sachusamu-c@users.noreply.github.com>
2021-09-06 08:54:09 +0530

    𝘴𝘴 ᴄͥʀⷮᴇᷤᴀⷨᴛⷷɪᴏɴ

--- plugins/git.js
+++ plugins/git.js
@@ -11,14 +11,19 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     
     
    
-  r_text[0] = "http://telegra.ph/file/68809ff84d7cd4447a76e.jpg";
+  r_text[0] = "https://www.pixelstalk.net/wp-content/uploads/2016/03/Pikachu-wallpaper-HD-dowload.jpg";
     
     
     var i = Math.floor(1*Math.random())
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*𝘴𝘴 ᴄͥʀⷮᴇᷤᴀⷨᴛⷷɪᴏɴ BOT CREATED BY SACHUSAMU* \n\n *「 ABOUT OWNER 」* \n\n\n *TELEGRAM CHANNEL:- https://t.me/joinchat/2oqiVrhkeXwyOTc1* \n *OWNER NUMBER:- _tinyurl.com/vab4v3es_* \n *GITHUB LINK :- _github.com/amal-dx_* \n\n *「 ABOUT BOT 」* \n\n\n *BOT setting video : https://imgur.com/gallery/alPJ0fY.mp4* \n\n *Githublink       https://github.com/amal-dx/DXTROX_V1* \n\n *Audio commads     https://github.com/amal-dx/DXTROX_V1/tree/master/uploads* 
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*creater Sachusamu*
+*owner number wa.me/919744474996*
+*bot setting video : https://youtu.be/_D4ZYuUSXjs
+*githublink       https://github.com/sachusamu-c/JINN*
+*audio commads    https://github.com/sachusamu-c/JINN/tree/master/uploads*
+*sticker commads  https://github.com/sachusamu-c/JINN/tree/master/uploads*
 `}) 
 
 }));
