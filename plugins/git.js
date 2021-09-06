cb02cfa76b92222d00c45adf6148852e7245bf48
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-09-06 10:20:38 +0530

    git

--- plugins/git.js
+++ plugins/git.js
@@ -3,27 +3,22 @@ const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
 const axios = require('axios');
 
 const Language = require('../language');
-const Lang = Language.getString('wallpaper');
+const Lang = Language.getString('gitlink');
 
-Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message, match) => {
+Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.GL}, (async (message, match) => {
 
     var r_text = new Array ();
     
     
    
-  r_text[0] = "20210729_050314.png";
+  r_text[0] = "https://i.imgur.com/Rg3zbHU.png";
     
     
     var i = Math.floor(1*Math.random())
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*creater Sachusamu*
-*owner number wa.me/919744474996*
-*bot setting video : https://youtu.be/_D4ZYuUSXjs
-*githublink       https://github.com/sachusamu-c/JINN*
-*audio commads    https://github.com/sachusamu-c/JINN/tree/master/uploads*
-*sticker commads  https://github.com/sachusamu-c/JINN/tree/master/uploads*
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*creater Sachusamu* \n\n\n *owner number wa.me/919744474996* \n *bot setting video : https://youtu.be/_D4ZYuUSXjs \n *githublink:-       https://github.com/sachusamu-c/JINN* \n *audio commands:-    https://github.com/sachusamu-c/JINN/tree/master/uploads* \n *sticker commands:-  https://github.com/sachusamu-c/JINN/tree/master/uploads*
 `}) 
 
 }));
