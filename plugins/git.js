b3c4205b5ad561351fa22ff5def2904fd25189e6
sachusamu-c <87406580+sachusamu-c@users.noreply.github.com>
2021-09-06 08:49:13 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -1,43 +1,24 @@
 const Asena = require('../events');
-
 const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
-
 const axios = require('axios');
 
 const Language = require('../language');
-
 const Lang = Language.getString('wallpaper');
 
 Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message, match) => {
 
     var r_text = new Array ();
-
     
-
     
-
    
-
-  r_text[0] = "https://ibb.co/5n9gdxj";
-
+  r_text[0] = "http://telegra.ph/file/68809ff84d7cd4447a76e.jpg";
     
-
     
-
     var i = Math.floor(1*Math.random())
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*creater SACHUSAMU*
-
-*owner number wa.me/919744474996*
-
-*bot setting video : https://youtu.be/RlSG4ZgPls0*
-
-*githublink : https://github.com/sachusamu-c/JINN*
-
-*audio commads : https://github.com/sachusamu-c/JINN/tree/master/uploads*
-
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*𝘴𝘴 ᴄͥʀⷮᴇᷤᴀⷨᴛⷷɪᴏɴ BOT CREATED BY SACHUSAMU* \n\n *「 ABOUT OWNER 」* \n\n\n *TELEGRAM CHANNEL:- https://t.me/joinchat/2oqiVrhkeXwyOTc1* \n *OWNER NUMBER:- _tinyurl.com/vab4v3es_* \n *GITHUB LINK :- _github.com/amal-dx_* \n\n *「 ABOUT BOT 」* \n\n\n *BOT setting video : https://imgur.com/gallery/alPJ0fY.mp4* \n\n *Githublink       https://github.com/amal-dx/DXTROX_V1* \n\n *Audio commads     https://github.com/amal-dx/DXTROX_V1/tree/master/uploads* 
 `}) 
 
 }));
